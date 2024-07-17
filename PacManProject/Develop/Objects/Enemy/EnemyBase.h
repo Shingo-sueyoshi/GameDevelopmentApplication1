46c6db73c7203834cbd9b61f410df33d9ad53e18
Shingo-sueyoshi <138412610+Shingo-sueyoshi@users.noreply.github.com>
2024-07-17 15:01:09 +0900

    a

--- PacManProject/Develop/Objects/Enemy/EnemyBase.h
+++ PacManProject/Develop/Objects/Enemy/EnemyBase.h
@@ -1,9 +1,31 @@
 ﻿#pragma once
 
 #include "../GameObject.h"
+#include "../../Utility/StageData.h"
 
+//エネミー状態
+enum eEnemyState
+{
+	IDLE,		//待機状態
+	MOVE,		//移動状態
+	DIE,		//死亡状態
+};
+
+
+/// <summary>
+/// エネミークラス（アカベエ）
+/// </summary>
 class EnemyBase : public GameObject
 {
+private:
+	//進行方向状態
+	enum eDirectionState : unsigned char
+	{
+		UP,
+		RIGHT,
+		DOWN,
+		LEFT,
+	};
 protected:
 
 public:
