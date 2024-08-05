5e10cc16daff6a4873d732ce48824da3602d489e
Shingo-sueyoshi <138412610+Shingo-sueyoshi@users.noreply.github.com>
2024-08-05 15:02:10 +0900

    aa

--- PacManProject/Develop/Objects/Enemy/EnemyBase.h
+++ PacManProject/Develop/Objects/Enemy/EnemyBase.h
@@ -6,9 +6,9 @@
 //エネミー状態
 enum eEnemyState
 {
-	IDLE,		//待機状態
-	MOVE,		//移動状態
-	DIE,		//イジケ状態
+	ENEMY_IDLE,		//待機状態
+	ENEMY_MOVE,		//移動状態
+	ENEMY_DIE,		//イジケ状態
 };
 
 
@@ -25,17 +25,27 @@ private:
 		RIGHT,
 		DOWN,
 		LEFT,
+		NONE
 	};
 protected:
 
 private:
-	std::vector<int> move_animation;
-	std::vector<int> dying_animation;
-	Vector2D velocity;
-	eEnemyState enemy_state;
-	eDirectionState now_direction_state;
-	eDirectionState next_direction_state;
-	
+	std::vector<int> move_animation;		// 移動のアニメーション画像
+	std::vector<int> dying_animation;		// 死亡のアニメーション画像
+	Vector2D old_location;					// 前回の location
+	Vector2D velocity;						// 移動量
+	eEnemyState enemy_state;				// プレイヤー状態
+	eDirectionState now_direction_state;	// 現在進行方向状態
+	eDirectionState next_direction_state;	// 次回進行方向状態
+	int food_count;							// 餌を食べた数
+	float animation_time;					// アニメーション時間
+	int animation_count;					// アニメーション添字
+	ePanelID old_panel;						// 前回パネル情報
+	bool is_power_up;						// パワー餌を食べたか？
+	bool is_destroy;						// 死んだ状態になったか？
+
+	// 移動アニメーションの順番
+	const int animation_num[4] = { 0, 1, 2, 1, };
 
 public:
 	EnemyBase();
@@ -52,11 +62,46 @@ public:
 	/// <param name="hit_object">当たったゲームオブジェクトのポインタ</param>
 	virtual void OnHitCollision(GameObjectBase* hit_object) override;
 
+public:
+	/// <summary>
+	/// 餌を食べた数取得
+	/// </summary>
+	/// <returns>餌を食べた数</returns>
+	int GetFoodCount() const;
+
 	/// <summary>
 	/// プレイヤーの状態を取得する
 	/// </summary>
 	/// <returns>プレイヤーの状態</returns>
 	eEnemyState GetEnemyState() const;
 
+	/// <summary>
+	/// プレイヤーがパワーアップしてるか確認する
+	/// </summary>
+	/// <returns>プレイヤーの状態</returns>
+	bool GetPowerUp() const;
 
+	/// <summary>
+	/// パワーダウンさせる
+	/// </summary>
+	void SetPowerDown();
+
+	/// <summary>
+	/// プレイヤーがパワーアップしてるか確認する
+	/// </summary>
+	/// <returns>プレイヤーの状態</returns>
+	bool GetDestroy() const;
+
+
+private:
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	/// <param name="delta_second">1フレームあたりの時間</param>
+	void Movement(float delta_second);
+	/// <summary>
+	/// アニメーション制御
+	/// </summary>
+	/// <param name="delta_second">1フレームあたりの時間</param>
+	void AnimationControl(float delta_second);
 };
